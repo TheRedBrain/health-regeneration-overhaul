8fb974c858981eb7a37dc972c39a162ddce265d4
TheRedBrain <thetrueredbrain@gmail.com>
2025-09-15 19:54:37 +0200

    made health attributes base values configurable

--- src/main/java/com/github/theredbrain/healthregenerationoverhaul/config/ServerConfig.java
+++ src/main/java/com/github/theredbrain/healthregenerationoverhaul/config/ServerConfig.java
@@ -3,6 +3,7 @@ package com.github.theredbrain.healthregenerationoverhaul.config;
 import com.github.theredbrain.healthregenerationoverhaul.HealthRegenerationOverhaul;
 import me.fzzyhmstrs.fzzy_config.annotations.ConvertFrom;
 import me.fzzyhmstrs.fzzy_config.config.Config;
+import me.fzzyhmstrs.fzzy_config.config.ConfigSection;
 
 @ConvertFrom(fileName = "server.json5", folder = "healthregenerationoverhaul")
 public class ServerConfig extends Config {
@@ -12,4 +13,14 @@ public class ServerConfig extends Config {
 	}
 
 	public boolean disable_vanilla_food_system = true;
+
+	public NaturalPlayerAttributeValuesSection naturalPlayerAttributeValues = new NaturalPlayerAttributeValuesSection();
+
+	public static class NaturalPlayerAttributeValuesSection extends ConfigSection {
+		public float natural_health_regeneration = 0.0F;
+		public float natural_max_health = 9.0F;
+		public float natural_health_regeneration_delay_threshold = 20.0F;
+		public float natural_health_tick_threshold = 20.0F;
+		public float natural_reserved_health = 0.0F;
+	}
 }
--- src/main/java/com/github/theredbrain/healthregenerationoverhaul/mixin/entity/player/PlayerEntityMixin.java
+++ src/main/java/com/github/theredbrain/healthregenerationoverhaul/mixin/entity/player/PlayerEntityMixin.java
@@ -5,8 +5,10 @@ import com.github.theredbrain.healthregenerationoverhaul.entity.HealthRegenerati
 import com.google.common.collect.HashMultimap;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.entry.RegistryEntry;
@@ -17,12 +19,22 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity implements HealthRegeneratingEntity {
 
 	protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
 		super(entityType, world);
+	}
+
+	@Inject(method = "createPlayerAttributes", at = @At("RETURN"))
+	private static void healthregenerationoverhaul$createPlayerAttributes(CallbackInfoReturnable<DefaultAttributeContainer.Builder> cir) {
+		cir.getReturnValue()
+				.add(EntityAttributes.GENERIC_MAX_HEALTH, 1.0)
+				.add(HealthRegenerationOverhaul.HEALTH_TICK_THRESHOLD, 0.0)
+				.add(HealthRegenerationOverhaul.HEALTH_REGENERATION_DELAY_THRESHOLD, 0.0)
+		;
 	}
 
 	@Inject(method = "applyDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;setHealth(F)V", shift = At.Shift.AFTER))
@@ -32,17 +44,19 @@ public abstract class PlayerEntityMixin extends LivingEntity implements HealthRe
 
 	@Inject(method = "tick", at = @At("TAIL"))
 	public void healthregenerationoverhaul$tick(CallbackInfo ci) {
-		if (this.getServer() != null && this.getWorld().getGameRules().getBoolean(GameRules.NATURAL_REGENERATION)) {
-			this.getAttributes().addTemporaryModifiers(getNaturalHealthRegenerationModifier());
-		} else {
-			this.getAttributes().removeModifiers(getNaturalHealthRegenerationModifier());
+		if (!this.getWorld().isClient()) {
+			this.getAttributes().addTemporaryModifiers(getNaturalAttributeModifiers());
 		}
 	}
 
 	@Unique
-	private HashMultimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> getNaturalHealthRegenerationModifier() {
+	private HashMultimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> getNaturalAttributeModifiers() {
 		HashMultimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> hashMultimap = HashMultimap.create();
-		hashMultimap.put(HealthRegenerationOverhaul.HEALTH_REGENERATION, new EntityAttributeModifier(HealthRegenerationOverhaul.identifier("natural_health_regeneration_modifier"), 1.0, EntityAttributeModifier.Operation.ADD_VALUE));
+		hashMultimap.put(HealthRegenerationOverhaul.HEALTH_REGENERATION, new EntityAttributeModifier(HealthRegenerationOverhaul.identifier("natural_health_regeneration_modifier"), HealthRegenerationOverhaul.SERVER_CONFIG.naturalPlayerAttributeValues.natural_health_regeneration + (this.getWorld().getGameRules().getBoolean(GameRules.NATURAL_REGENERATION) ? 1.0 : 0.0), EntityAttributeModifier.Operation.ADD_VALUE));
+		hashMultimap.put(HealthRegenerationOverhaul.HEALTH_REGENERATION, new EntityAttributeModifier(HealthRegenerationOverhaul.identifier("natural_max_health_modifier"), HealthRegenerationOverhaul.SERVER_CONFIG.naturalPlayerAttributeValues.natural_max_health, EntityAttributeModifier.Operation.ADD_VALUE));
+		hashMultimap.put(HealthRegenerationOverhaul.HEALTH_TICK_THRESHOLD, new EntityAttributeModifier(HealthRegenerationOverhaul.identifier("natural_health_tick_threshold_modifier"), HealthRegenerationOverhaul.SERVER_CONFIG.naturalPlayerAttributeValues.natural_health_regeneration_delay_threshold, EntityAttributeModifier.Operation.ADD_VALUE));
+		hashMultimap.put(HealthRegenerationOverhaul.HEALTH_REGENERATION_DELAY_THRESHOLD, new EntityAttributeModifier(HealthRegenerationOverhaul.identifier("natural_health_regeneration_delay_threshold_modifier"), HealthRegenerationOverhaul.SERVER_CONFIG.naturalPlayerAttributeValues.natural_health_tick_threshold, EntityAttributeModifier.Operation.ADD_VALUE));
+		hashMultimap.put(HealthRegenerationOverhaul.RESERVED_HEALTH, new EntityAttributeModifier(HealthRegenerationOverhaul.identifier("natural_reserved_health_modifier"), HealthRegenerationOverhaul.SERVER_CONFIG.naturalPlayerAttributeValues.natural_reserved_health, EntityAttributeModifier.Operation.ADD_VALUE));
 		return hashMultimap;
 	}
 }
