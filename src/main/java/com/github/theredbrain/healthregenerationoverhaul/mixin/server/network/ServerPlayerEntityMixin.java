330e7ba449d2a6a95233ebac812d5c95fb3af1a8
TheRedBrain <thetrueredbrain@gmail.com>
2025-10-04 21:26:45 +0200

    fixed first join health amount

--- src/main/java/com/github/theredbrain/healthregenerationoverhaul/mixin/server/network/ServerPlayerEntityMixin.java
+++ src/main/java/com/github/theredbrain/healthregenerationoverhaul/mixin/server/network/ServerPlayerEntityMixin.java
@@ -4,15 +4,20 @@ import com.github.theredbrain.healthregenerationoverhaul.entity.HealthRegenerati
 import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.ServerStatHandler;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity implements HealthRegeneratingEntity {
+
+	@Shadow public abstract ServerStatHandler getStatHandler();
 
 	public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
 		super(world, pos, yaw, gameProfile);
@@ -21,6 +26,9 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity implements He
 	@Inject(method = "onSpawn", at = @At("TAIL"))
 	public void healthregenerationoverhaul$onSpawn(CallbackInfo ci) {
 		this.healthregenerationoverhaul$setApplyOldHealth(false);
+		if (this.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.LEAVE_GAME)) <= 0) {
+			this.healthregenerationoverhaul$setApplyMaxHealth(true);
+		}
 	}
 
 }
